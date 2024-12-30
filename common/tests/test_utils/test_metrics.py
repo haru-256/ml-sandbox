4a1dd597c428b287cf0ab8dccbdfa5b5faf37c80
Yohei Okabayashi <yohei.kuro48@gmail.com>
2024-12-30 11:55:27 +0900

    ADD: update pyproject and mrr, hit_rate

--- common/tests/test_utils/test_metrics.py
+++ common/tests/test_utils/test_metrics.py
@@ -1,6 +1,13 @@
 import torch
 
-from utils.metrics import create_classification_inputs, create_retrieval_inputs
+from utils.metrics import (
+    create_classification_inputs,
+    create_retrieval_inputs,
+    hit_rate_v1,
+    hit_rate_v2,
+    mrr_v1,
+    mrr_v2,
+)
 
 
 def test_create_classification_inputs():
@@ -27,3 +34,65 @@ def test_create_retrieval_inputs():
     torch.testing.assert_close(actual_logits, expected_logits)
     torch.testing.assert_close(actual_target, excepted_target)
     torch.testing.assert_close(actual_indexes, expected_indexes)
+
+
+def test_mrr():
+    score = torch.tensor(
+        [
+            [0.1, 0.2, 0.3, 0.4],
+            [0.4, 0.3, 0.2, 0.1],
+        ]
+    )
+    target = torch.tensor(
+        [
+            [1, 0, 0, 0],
+            [1, 0, 0, 0],
+        ]
+    ).long()
+
+    expected = torch.tensor([0.0, 1.0]).mean()
+    actual = mrr_v1(score, target, k=1)
+    torch.testing.assert_close(actual, expected)
+
+    expected = torch.tensor([0.0, 1.0]).mean()
+    actual = mrr_v2(score, target, k=1)
+    torch.testing.assert_close(actual, expected)
+
+    expected = torch.tensor([1.0 / 4, 1.0]).mean()
+    actual = mrr_v1(score, target, k=4)
+    torch.testing.assert_close(actual, expected)
+
+    expected = torch.tensor([1.0 / 4, 1.0]).mean()
+    actual = mrr_v2(score, target, k=4)
+    torch.testing.assert_close(actual, expected)
+
+
+def test_hit_rate():
+    score = torch.tensor(
+        [
+            [0.1, 0.2, 0.3, 0.4],
+            [0.4, 0.3, 0.2, 0.1],
+        ]
+    )
+    target = torch.tensor(
+        [
+            [1, 0, 0, 0],
+            [1, 0, 0, 0],
+        ]
+    ).long()
+
+    expected = torch.tensor([0.0, 1.0]).mean()
+    actual = hit_rate_v1(score, target, k=1)
+    torch.testing.assert_close(actual, expected)
+
+    expected = torch.tensor([0.0, 1.0]).mean()
+    actual = hit_rate_v2(score, target, k=1)
+    torch.testing.assert_close(actual, expected)
+
+    expected = torch.tensor([1.0, 1.0]).mean()
+    actual = hit_rate_v1(score, target, k=4)
+    torch.testing.assert_close(actual, expected)
+
+    expected = torch.tensor([1.0, 1.0]).mean()
+    actual = hit_rate_v2(score, target, k=4)
+    torch.testing.assert_close(actual, expected)
--- common/utils/metrics.py
+++ common/utils/metrics.py
@@ -1,4 +1,5 @@
 import torch
+from torchmetrics.functional.retrieval import retrieval_hit_rate, retrieval_reciprocal_rank
 
 
 def create_classification_inputs(
@@ -38,3 +39,115 @@ def create_retrieval_inputs(
     batch_size, num_samples = score.size()
     indexes = torch.arange(batch_size).reshape(batch_size, 1).expand(batch_size, num_samples).long()
     return score, target, indexes
+
+
+def mrr(score: torch.Tensor, target: torch.Tensor, k: int = 10) -> torch.Tensor:
+    """mean reciprocal rank at k
+
+    Args:
+        score: logits, shape (batch_size, num_samples)
+        target: target long: 1 is positive and 0 is negative, shape (batch_size, num_samples)
+        k: top k
+
+    Returns:
+        mrr: mean reciprocal rank at k
+    """
+    return mrr_v1(score, target, k)
+
+
+def mrr_v1(score: torch.Tensor, target: torch.Tensor, k: int = 10) -> torch.Tensor:
+    """mean reciprocal rank at k by myself
+
+    Args:
+        score: logits, shape (batch_size, num_samples)
+        target: target long: 1 is positive and 0 is negative, shape (batch_size, num_samples)
+        k: top k
+
+    Returns:
+        mrr: mean reciprocal rank at k
+    """
+    assert score.size() == target.size() and score.ndim == 2
+    assert k > 0 and k <= score.size(1)
+
+    _, indexes = score.topk(k, dim=1, largest=True, sorted=True)
+    rel = torch.take_along_dim(target, indexes, dim=1)
+    rank = rel.argmax(dim=1)
+    values = 1.0 / (rank.float() + 1.0)
+    zeros_mask = rel.sum(dim=1) == 0
+    values[zeros_mask] = 0.0  # if no positive sample, mrr is 0
+    return values.mean()
+
+
+def mrr_v2(score: torch.Tensor, target: torch.Tensor, k: int = 10) -> torch.Tensor:
+    """mean reciprocal rank at k by torchmetrics. This is slower than mrr_v1, because it uses for loop.
+
+    Args:
+        score: logits, shape (batch_size, num_samples)
+        target: target long: 1 is positive and 0 is negative, shape (batch_size, num_samples)
+        k: top k
+
+    Returns:
+        mrr: mean reciprocal rank at k
+    """
+    assert score.size() == target.size() and score.ndim == 2
+    assert k > 0 and k <= score.size(1)
+
+    values = torch.as_tensor(
+        [retrieval_reciprocal_rank(s, t, top_k=k) for s, t in zip(score, target)],
+        dtype=torch.float32,
+    )
+    return values.mean()
+
+
+def hit_rate(score: torch.Tensor, target: torch.Tensor, k: int = 10) -> torch.Tensor:
+    """hit rate at k
+
+    Args:
+        score: logits, shape (batch_size, num_samples)
+        target: target long, shape (batch_size, num_samples)
+        k: top k
+
+    Returns:
+        hit_rate: hit rate at k
+    """
+    return hit_rate_v1(score, target, k)
+
+
+def hit_rate_v1(score: torch.Tensor, target: torch.Tensor, k: int = 10) -> torch.Tensor:
+    """hit rate at k by myself
+
+    Args:
+        score: logits, shape (batch_size, num_samples)
+        target: target long, shape (batch_size, num_samples)
+        k: top k
+
+    Returns:
+        hit_rate: hit rate at k
+    """
+    assert score.size() == target.size() and score.ndim == 2
+    assert k > 0 and k <= score.size(1)
+
+    _, indexes = score.topk(k, dim=1, largest=True, sorted=True)
+    rel = torch.take_along_dim(target, indexes, dim=1)
+    return (rel.sum(dim=1) > 0).float().mean()
+
+
+def hit_rate_v2(score: torch.Tensor, target: torch.Tensor, k: int = 10) -> torch.Tensor:
+    """hit rate at k by myself
+
+    Args:
+        score: logits, shape (batch_size, num_samples)
+        target: target long, shape (batch_size, num_samples)
+        k: top k
+
+    Returns:
+        hit_rate: hit rate at k
+    """
+    assert score.size() == target.size() and score.ndim == 2
+    assert k > 0 and k <= score.size(1)
+
+    values = torch.as_tensor(
+        [retrieval_hit_rate(s, t, top_k=k) for s, t in zip(score, target)],
+        dtype=torch.float32,
+    )
+    return values.mean()
