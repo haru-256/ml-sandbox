cc0bd38bf2554352309acabdcac7ca0f525e27af
Yohei Okabayashi <yohei.kuro48@gmail.com>
2025-07-15 22:25:28 +0900

    change

--- libs/ml_sandbox_libs/src/ml_sandbox_libs/data/amazon_reviews_dataset.py
+++ libs/ml_sandbox_libs/src/ml_sandbox_libs/data/amazon_reviews_dataset.py
@@ -808,8 +808,6 @@ def bipartite_graph_preprocess_dataset(
     dataset_dict: D.DatasetDict, metadata: D.Dataset
 ) -> tuple[
     pl.DataFrame,
-    pl.DataFrame,
-    pl.DataFrame,
     dict[str, int],
     dict[str, int],
     dict[str, int],
@@ -839,13 +837,20 @@ def bipartite_graph_preprocess_dataset(
     )
 
     def _preprocess(df: pl.DataFrame) -> pl.DataFrame:
-        main_df = df.select(["user_id", "parent_asin", "category", "rating", "timestamp"])
+        main_df = df.select(["split", "user_id", "parent_asin", "category", "rating", "timestamp"])
         # add index columns
         main_df = main_df.join(user2index_df, on="user_id", how="left", validate="m:1")
         main_df = main_df.join(item2index_df, on="parent_asin", how="left", validate="m:1")
         # add metadata
         main_df = main_df.join(category2index_df, on="category", how="left", validate="m:1")
+        main_df = main_df.with_columns(
+            pl.col("user_index").fill_null(SpecialIndex.UNK).alias("user_index"),
+            pl.col("item_index").fill_null(SpecialIndex.UNK).alias("item_index"),
+            pl.col("category").fill_null("#UNK").alias("category"),
+            pl.col("category_index").fill_null(SpecialIndex.UNK).alias("category_index"),
+        )
         main_df = main_df.group_by("user_id", "parent_asin").agg(
+            pl.max("split").alias("split"),
             pl.max("user_index").alias("user_index"),
             pl.max("item_index").alias("item_index"),
             pl.max("category").alias("category"),
@@ -854,8 +859,13 @@ def bipartite_graph_preprocess_dataset(
             pl.min("timestamp").alias("timestamp"),
             pl.len().alias("num_ratings"),
         )
+        if (main_df["num_ratings"] != 1).any():
+            raise ValueError(
+                "There are multiple ratings for the same user and item. This is not supported for bipartite graph."
+            )
         main_df = main_df.select(
             [
+                "split",
                 "user_id",
                 "user_index",
                 "parent_asin",
@@ -870,14 +880,17 @@ def bipartite_graph_preprocess_dataset(
         return main_df
 
     logger.info("Preprocessing the dataset for bipartite graph")
-    train_df = _preprocess(train_df)
-    val_df = _preprocess(val_df)
-    test_df = _preprocess(test_df)
+    all_df = pl.concat(
+        [
+            train_df.with_columns(pl.lit("train").alias("split")),
+            val_df.with_columns(pl.lit("valid").alias("split")),
+            test_df.with_columns(pl.lit("test").alias("split")),
+        ]
+    )
+    all_df = _preprocess(all_df)
 
     return (
-        train_df,
-        val_df,
-        test_df,
+        all_df,
         user2index,
         item2index,
         category2index,
