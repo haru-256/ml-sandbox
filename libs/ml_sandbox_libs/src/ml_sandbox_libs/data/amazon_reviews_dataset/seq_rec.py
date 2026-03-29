73e9a39e11f4fe96b7ff63521c0923a727f0632a
Yohei Okabayashi <yohei.kuro48@gmail.com>
2026-03-29 18:22:30 +0900

    review対応

--- libs/ml_sandbox_libs/src/ml_sandbox_libs/data/amazon_reviews_dataset/seq_rec.py
+++ libs/ml_sandbox_libs/src/ml_sandbox_libs/data/amazon_reviews_dataset/seq_rec.py
@@ -596,8 +596,6 @@ class AmazonReviewsSeqRecDataModule(L.LightningDataModule):
             }
         )
 
-        logger.info(self.summary())
-
     def setup(self, stage: str) -> None:
         if stage == "fit":
             self.train_dataset = AmazonReviewsSeqRecDataset(
@@ -613,6 +611,7 @@ class AmazonReviewsSeqRecDataModule(L.LightningDataModule):
                 neg_sample_size=self.eval_negative_sample_size,
                 max_seq_len=self.max_seq_len,
             )
+            logger.info(self.summary())
         elif stage == "test":
             # NOTE: For ranking metrics, we need to sample more negative items.
             self.test_dataset = AmazonReviewsSeqRecDataset(
